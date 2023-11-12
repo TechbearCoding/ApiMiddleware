4f0c08ac20473452b421673eed95bf54514fb225
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2023-11-13 01:02:37 +0200

    Initial commit

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,25 @@
+﻿using ApiMiddleware.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMiddleware.Controllers
+{
+    [ApiController]
+    [Route("post")]
+    public class ContentController : ControllerBase
+    {
+        private static List<ContentEntity> contentEntities = new List<ContentEntity>()
+        {
+            new ContentEntity("1", "this is title", "this is post"),
+            new ContentEntity("2", "this is another title", "this is another post"),
+            new ContentEntity("3", "this is yet another title", "this is yet another post"),
+        };
+
+        [HttpGet(Name = "GetContent")]
+        public IEnumerable<ContentEntity> Get()
+        {
+            return contentEntities;
+        }
+     
+    }
+}
---
+++ Entity/ContentEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiMiddleware.Entity
+{
+    public class ContentEntity
+    {
+        public String Id { get; set; }
+        public String PostTitle { get; set; }
+        public String PostContent { get; set; }
+        public ContentEntity(String id, String postTitle, String postContent) 
+        {
+            Id = id;
+            PostTitle = postTitle;
+            PostContent = postContent;
+        }  
+    }
+}
