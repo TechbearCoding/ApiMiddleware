827998c32ad8e50cc035a2861053e6c059b49391
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2023-11-13 22:51:34 +0200

    Added migration code

--- Entity/ContentEntity.cs
+++ Entity/ContentEntity.cs
@@ -1,5 +1,23 @@
-﻿namespace ApiMiddleware.Entity
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiMiddleware.Entity
 {
+    public class ContentContext : DbContext
+    {
+        public String DbPath { get; private set; }
+
+        public ContentContext() : base()
+        {
+            DbPath = "Content.db";
+        }
+
+        public DbSet<ContentEntity> ContentEntities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename={DbPath}");
+        }
+    }   
     public class ContentEntity
     {
         public String Id { get; set; }
